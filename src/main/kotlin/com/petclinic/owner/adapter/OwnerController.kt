2eb528c3ac422e78b5df17ecf33c859951a3afc2
Richard.AmoakoAgyei <richard.amoakoagyei@target.com>
2020-06-12 19:37:31 -0500

    Swagger Docs

--- src/main/kotlin/com/petclinic/owner/adapter/OwnerController.kt
+++ src/main/kotlin/com/petclinic/owner/adapter/OwnerController.kt
@@ -6,6 +6,7 @@ import com.petclinic.owner.model.Owner
 import com.petclinic.owner.model.OwnerResponse
 import com.petclinic.owner.service.OwnerService
 import com.petclinic.owner.service.PetService
+import io.swagger.v3.oas.annotations.Operation
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
@@ -21,17 +22,20 @@ class OwnerController(val ownerService: OwnerService, val petService: PetService
         private val logger = LoggerFactory.getLogger("ProductController")
     }
 
-    //    @Operation(hidden = true, summary = "Ignore this method from being publicly documented")
+    @Operation(description = "Creates an owner in the platform if the owner does not exist. The required" +
+            "unique key is a telephone. No two owners will have the same phonenumber.",
+            summary = "create an owner")
     @PostMapping(value = ["owner", "/owner"], produces = [MediaType.APPLICATION_JSON_VALUE], consumes = [MediaType.APPLICATION_JSON_VALUE])
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@Valid @RequestBody request: CreateOwnerRequest): Mono<OwnerResponse> {
-
         val owner = createOwner(request)
-
         return ownerService.save(owner)
                 .map { res -> createOwnerResponse(res) }
     }
 
+    @Operation(description = "Creates a list of owners in the platform if the owner does not exist. The required" +
+            "unique key is a telephone. No two owners will have the same phonenumber.",
+            summary = "create an owner")
     @PostMapping(value = ["owners", "/owners"], produces = [MediaType.APPLICATION_JSON_VALUE], consumes = [MediaType
             .APPLICATION_JSON_VALUE])
     @ResponseStatus(HttpStatus.CREATED)
@@ -43,30 +47,17 @@ class OwnerController(val ownerService: OwnerService, val petService: PetService
                 .map { res -> createOwnerResponse(res) }
     }
 
-    private fun createOwner(request: CreateOwnerRequest): Owner {
-        return Owner(request.id, request.lastName, request.firstName, request.telephone, request.address, request.city)
-    }
 
-    private fun createOwnerResponse(owner: Owner): OwnerResponse {
-        if (owner.pets.isNotEmpty()) {
-            val petResponses = owner.pets
-                    .map { pet -> PetResponse(pet) }
-                    .toSet()
-            return OwnerResponse(owner, petResponses)
-        }
-        return OwnerResponse(owner)
-    }
-
-
-    //    @Operation(hidden = true, summary = "Ignore this method from being publicly documented")
+    @Operation(description = "returns all owners that are available in the system",
+            summary = "find all owners")
     @GetMapping(value = ["owners", "owners/"], produces = [MediaType.APPLICATION_JSON_VALUE], consumes = [MediaType.APPLICATION_JSON_VALUE])
     fun findOwners(): Flux<OwnerResponse> {
-
         return ownerService.findAll()
                 .map { o -> createOwnerResponse(o) }
     }
 
-    //    @Operation(hidden = true, summary = "Ignore this method from being publicly documented")
+    @Operation(description = "find an owner by the telephone",
+            summary = "find owner by telephone")
     @GetMapping(value = ["owner", "owner/"], produces = [MediaType.APPLICATION_JSON_VALUE], consumes = [MediaType
             .APPLICATION_JSON_VALUE])
     fun findOwner(@RequestParam("telephone") telephone: String?): Mono<OwnerResponse> {
@@ -77,7 +68,8 @@ class OwnerController(val ownerService: OwnerService, val petService: PetService
         return Mono.empty()
     }
 
-    //    @Operation(hidden = true, summary = "Ignore this method from being publicly documented")
+    @Operation(description = "find owner by id",
+            summary = "find owner by id")
     @GetMapping(value = ["owner/{id}"], produces = [MediaType.APPLICATION_JSON_VALUE], consumes = [MediaType
             .APPLICATION_JSON_VALUE])
     fun findOwnerById(@PathVariable("id") id: String, @RequestParam("include_pets") includePets: Boolean?): Mono<OwnerResponse> {
@@ -110,5 +102,19 @@ class OwnerController(val ownerService: OwnerService, val petService: PetService
                     }
         }
         return maybeOwner.map { res -> createOwnerResponse(res) }
+    }
+
+    private fun createOwner(request: CreateOwnerRequest): Owner {
+        return Owner(request.id, request.lastName, request.firstName, request.telephone, request.address, request.city)
+    }
+
+    private fun createOwnerResponse(owner: Owner): OwnerResponse {
+        if (owner.pets.isNotEmpty()) {
+            val petResponses = owner.pets
+                    .map { pet -> PetResponse(pet) }
+                    .toSet()
+            return OwnerResponse(owner, petResponses)
+        }
+        return OwnerResponse(owner)
     }
 }
