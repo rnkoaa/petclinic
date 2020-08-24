df5ed8990884a05f596ab056f818225f18ef48c8
Richard.AmoakoAgyei <richard.amoakoagyei@target.com>
2020-08-23 19:38:00 -0500

    [WIP] Convert owner and Pet to use cassandra

--- backend/src/main/kotlin/com/petclinic/owner/repository/OwnerRepository.kt
+++ backend/src/main/kotlin/com/petclinic/owner/repository/OwnerRepository.kt
@@ -1,87 +1,20 @@
 package com.petclinic.owner.repository
 
-import com.google.api.core.ApiFuture
-import com.google.api.core.ApiFutures
-import com.google.cloud.firestore.DocumentSnapshot
-import com.google.cloud.firestore.Firestore
-import com.google.cloud.firestore.QuerySnapshot
-import com.google.cloud.firestore.WriteResult
-import com.petclinic.common.ApiFutureCallbackImpl
-import com.petclinic.common.ApiFutureDocumentSnapshotCallbackImpl
-import com.petclinic.common.ApiFutureQuerySnapshotCallbackImpl
-import com.petclinic.common.repository.BaseRepository
+
 import com.petclinic.owner.model.Owner
-import org.springframework.stereotype.Component
-import reactor.core.publisher.Flux
+import com.petclinic.owner.model.OwnerByTelephone
+import com.petclinic.owner.model.OwnerByTelephoneKey
+import org.springframework.data.cassandra.repository.Query
+import org.springframework.data.cassandra.repository.ReactiveCassandraRepository
 import reactor.core.publisher.Mono
 import java.util.*
-import java.util.concurrent.Executors
 
+interface OwnerRepository : ReactiveCassandraRepository<Owner, UUID> {
 
-interface OwnerRepository : BaseRepository<Owner, UUID> {
-    fun findByTelephone(telephone: String): Mono<Owner>
 }
 
-@Component("ownerRepository")
-class OwnerRepositoryImpl(val firestore: Firestore) : OwnerRepository {
-    val executor = Executors.newScheduledThreadPool(2)
-    override fun findByTelephone(telephone: String): Mono<Owner> {
-        val ownerCollection = firestore.collection("owner")
-        val querySnapshot = ownerCollection.whereEqualTo("telephone", telephone).get()
-        return Mono.create<QuerySnapshot> { monoSink ->
-            ApiFutures.addCallback(querySnapshot, ApiFutureQuerySnapshotCallbackImpl(monoSink), executor)
-        }.filter { it.documents.size > 0 }
-                .map { it.documents[0] }
-                .map { doc ->
-                    Owner(UUID.fromString(doc.id), doc["last_name"] as String, doc["first_name"] as String, doc["telephone"] as String,
-                            doc["address"] as String, doc["city"] as String)
-                }
+interface OwnerByTelephoneRepository : ReactiveCassandraRepository<OwnerByTelephone, OwnerByTelephoneKey> {
 
-    }
-
-    override fun save(entity: Owner): Mono<Owner> {
-        val ownerWithId = if (!entity.isNew()) entity else entity.copy(id = UUID.randomUUID())
-        val document = firestore.collection("owner").document(ownerWithId.id!!.toString())
-        val data = mutableMapOf<String, Any?>()
-        data["owner_id"] = ownerWithId.id
-        data["first_name"] = ownerWithId.firstName
-        data["last_name"] = ownerWithId.lastName
-        data["telephone"] = ownerWithId.telephone
-        data["address"] = ownerWithId.address
-        data["city"] = ownerWithId.city
-        val resultFuture: ApiFuture<WriteResult> = document.set(data)
-
-        return Mono.create<WriteResult> { sink ->
-            ApiFutures.addCallback(resultFuture, ApiFutureCallbackImpl(sink), executor)
-        }.map { ownerWithId }
-    }
-
-    override fun save(entities: List<Owner>): Flux<Owner> {
-        val savedOwners = entities.map { save(it) }
-        return Flux.mergeSequential(savedOwners)
-    }
-
-    override fun findById(id: UUID): Mono<Owner> {
-        val documentFuture = firestore.collection("owner").document(id.toString()).get()
-        return Mono.create<DocumentSnapshot> { monoSink ->
-            ApiFutures.addCallback(documentFuture, ApiFutureDocumentSnapshotCallbackImpl(monoSink), executor)
-        }.map { doc ->
-            Owner(UUID.fromString(doc.id), doc["last_name"] as String, doc["first_name"] as String, doc["telephone"] as String,
-                    doc["address"] as String, doc["city"] as String)
-        }
-    }
-
-    override fun findAll(): Flux<Owner> {
-        val querySnapshot = firestore.collection("owner").get()
-
-        return Mono.create<QuerySnapshot> { monoSink ->
-            ApiFutures.addCallback(querySnapshot, ApiFutureQuerySnapshotCallbackImpl(monoSink), executor)
-        }.filter { it.documents.size > 0 }
-                .map { it.documents }
-                .flatMapIterable { it }
-                .map { doc ->
-                    Owner(UUID.fromString(doc.id), doc["last_name"] as String, doc["first_name"] as String, doc["telephone"] as String,
-                            doc["address"] as String, doc["city"] as String)
-                }
-    }
+    @Query("select * from owner_by_telephone where telephone = ?0")
+    fun findByTelephone(telephone: String): Mono<OwnerByTelephone>
 }
